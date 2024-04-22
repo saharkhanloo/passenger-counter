6aaa1e80d07b349c9934ad4554de18ed685e1fdb
saharkhanloo <150781530+saharkhanloo@users.noreply.github.com>
2024-04-22 08:11:40 +0330

    Add files via upload
    That is amazing app to count passengers. Coding with Html, css , js.

---
+++ project1/index.js
@@ -0,0 +1,20 @@
+// document.getElementById("count").innerHTML = 5;
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+
+console.log(countEl);
+
+let count = 0;
+
+function increment() {
+  count += 1;
+  countEl.textContent = count;
+}
+function save() {
+  let countStr = count + " - ";
+
+  saveEl.textContent += countStr;
+  countEl.textContent = 0;
+  count = 0;
+  console.log(count);
+}
